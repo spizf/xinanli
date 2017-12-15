5ed39efe76e81c51dc3425b1250b14842fdc4de5
sxl <1542273951@qq.com>
2017-12-15 10:56:41 +0800

    后台关注链接修复

--- public/themes/admin/partials/managesidebar.blade.php
+++ public/themes/admin/partials/managesidebar.blade.php
@@ -1,5 +1,5 @@
 ﻿@foreach(Theme::get('manageMenu') as $v)
-    <li class="{{ in_array($v['id'],Theme::get('menu_ids'))?'active':'' }}">
+    <li class="{{ is_array(Theme::get('menu_ids'))&&in_array($v['id'],Theme::get('menu_ids'))?'active':'' }}">
         <a href="{!! empty($v['route'])?'javaascript:;':url($v['route']) !!}" class="{!! empty($v['_child'])?'':'dropdown-toggle' !!}">
             <i class="menu-icon fa {{ (!empty(Theme::get('menuIcon')[$v['name']]))?Theme::get('menuIcon')[$v['name']]:'fa-line-chart' }}"></i>
             <span class="menu-text"> {!! $v['name'] !!} </span>
@@ -11,7 +11,7 @@
         @if(!empty($v['_child']))
         <ul class="submenu">
             @foreach($v['_child'] as $value)
-            <li class="{{ in_array($value['id'],Theme::get('menu_ids'))?'active':'' }}">
+            <li class="{{ is_array(Theme::get('menu_ids'))&&in_array($value['id'],Theme::get('menu_ids'))?'active':'' }}">
                 <a href="{!! (empty($value['route']) && !empty($value['_child']))?'javaascript:;':url($value['route']) !!}" class="{!! empty($value['_child'])?'':'dropdown-toggle' !!}">
                     <i class="menu-icon fa fa-caret-right"></i>
                     {{ $value['name'] }}
@@ -23,7 +23,7 @@
                 @if(!empty($value['_child']))
                 <ul class="submenu">
                     @foreach($value['_child'] as $menus)
-                    <li class="{{ in_array($menus['id'],Theme::get('menu_ids'))?'active':'' }}">
+                    <li class="{{ is_array(Theme::get('menu_ids'))&&in_array($menus['id'],Theme::get('menu_ids'))?'active':'' }}">
                         <a href="{!! empty($menus['route'])?'javaascript:;':url($menus['route']) !!}" >
                             <i class="menu-icon fa fa-caret-right"></i>
                             {{ $menus['name'] }}
