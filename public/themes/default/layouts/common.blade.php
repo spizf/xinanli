764454ca08511622cd45100e89ae46718edef817
sxl <1542273951@qq.com>
2018-03-15 15:49:27 +0800

    导航弹窗效果

--- public/themes/default/layouts/common.blade.php
+++ public/themes/default/layouts/common.blade.php
@@ -82,9 +82,9 @@
         });
 
         obj.hover(function(){
-            $(".gongju_menu").show();
+            $(".gongju_menu").addClass("z-hover");
         },function(){
-            $(".gongju_menu").hide();
+            $(".gongju_menu").removeClass("z-hover");
         });
 
 
